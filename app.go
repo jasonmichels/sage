08a6f66c889e165ef447d1b46b61754071b9cfba
Jason Michels <thebizztech@me.com>
2016-01-03 22:38:10 -0600

    Started a sample reverse proxy

--- app.go
+++ app.go
@@ -1,9 +1,48 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"strings"
+)
+
+const (
+	SERVER1 = "https://www.fisdap.net/"
+	SERVER2 = "http://haproxy-production.fisdap.svc.tutum.io/"
 )
 
 func main() {
-	fmt.Println("Welcome to the Sage Proxy")
+	// get /blog => to server1
+	// get /login => to server2
+	var target1, target2 *url.URL
+	var err error
+
+	if target1, err = url.Parse(SERVER1); err != nil {
+		log.Fatal("parse url: ", err)
+	}
+
+	if target2, err = url.Parse(SERVER2); err != nil {
+		log.Fatal("parse url: ", err)
+	}
+
+	reverseProxy := new(httputil.ReverseProxy)
+
+	reverseProxy.Director = func(req *http.Request) {
+		req.URL.Scheme = "http"
+
+		if strings.HasPrefix(req.URL.Path, "/blog") {
+			req.URL.Host = target1.Host
+		}
+
+		if strings.HasPrefix(req.URL.Path, "/login") {
+			req.URL.Host = target2.Host
+		}
+	}
+
+	err = http.ListenAndServe(":80", reverseProxy)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
